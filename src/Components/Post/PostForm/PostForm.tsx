b49567dc735ee627ea22556e631e93bdc49a5ca9
reesbritt <reesbritt@hotmail.co.uk>
2020-08-23 11:57:47 +0100

    Beginning to hookup the post form

--- src/Components/Post/PostForm/PostForm.tsx
+++ src/Components/Post/PostForm/PostForm.tsx
@@ -1,85 +1,115 @@
-import React, { useState } from 'react';
-import { HeaderInput, HeadingText, HeadingContainer, DescriptionContainer, DescriptionText, DescriptionInput, ButtonWrapper } from './PostForm.components';
-import { Container, Row, Col } from 'react-bootstrap';
-import { Navbar } from '../../Navbar/Navbar';
-import { CallToAction } from '../../Button/Button.components';
+import React, { useState } from "react";
+import {
+  HeaderInput,
+  HeadingText,
+  HeadingContainer,
+  DescriptionContainer,
+  DescriptionText,
+  DescriptionInput,
+  ButtonWrapper,
+} from "./PostForm.components";
+import { Container, Row, Col } from "react-bootstrap";
+import { Navbar } from "../../Navbar/Navbar";
+import { CallToAction } from "../../Button/Button.components";
+import axios from "axios";
 
 const PostForm = () => {
-    const [name, setName] = useState('');
-    const [role, setRole] = useState('');
-    const [business, setBusiness] = useState('');
-    const [description, setDescription] = useState('')
+  const [post_author] = useState("");
+  const [target_audience] = useState("");
+  const [idea_description] = useState("");
+  const [long_text] = useState("");
+  const [pronoun] = useState("");
+  const [tags] = useState("");
+  const [category] = useState("");
+  const [external_link] = useState("");
 
-    const onBusinessChange = (e: any) => {
-        setBusiness(e.target.value);
-    }
-    const onRoleChange = (e: any) => {
-        setRole(e.target.value);
-    }
-    const onNameChange = (e: any) => {
-        setName(e.target.value);
+  const [payload, updatePayload] = useState({});
+
+  //   {"idea_description": "",
+  //                                      "target_audience": @minValidPostJson[:target_audience],
+  //                                      "pronoun": @minValidPostJson[:pronoun],
+  //                                      "post_author": @minValidPostJson[:post_author],
+  //                                      "category": @minValidPostJson[:category]}}
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newPayload = {
+      ...payload,
+      [e.currentTarget.name]: e.currentTarget.value,
     };
+    updatePayload(newPayload);
+  };
 
-    const onDescriptionChange = (e: any) => {
-        setDescription(e.target.value);
-    }
+  const submitFunction = (payload: any) => {
+    axios.post("http://localhost:3001/post", { newPost: payload });
+  };
 
-    return (
-        <Container>
-            <Navbar/>
-            <Row>
-                <Col>
-                    <HeadingContainer>
-                        <HeadingText>Hey, we're{" "}
-                            <HeaderInput 
-                                value={name} 
-                                onChange={onNameChange} 
-                                placeholder="Name or Company name" 
-                                size={name.length}
-                                maxLength={20}
-                            />
-                            And we are looking for{" "}
-                            <HeaderInput 
-                                value={role} 
-                                onChange={onRoleChange} 
-                                size={role.length}
-                                placeholder="designer, developer, artists..."
-                                maxLength={20}
-                            />
-                                for our{" "}
-                            <HeaderInput 
-                                value={business} 
-                                onChange={onBusinessChange} 
-                                size={business.length}
-                                placeholder="your idea I.E our charity"
-                                maxLength={20}
-                            />
-                        </HeadingText>
-                    </HeadingContainer>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <DescriptionContainer>
-                        <DescriptionInput
-                            value={description}
-                            onChange={onDescriptionChange}
-                            cols={40}
-                            rows={5}
-                            placeholder="Add any additional information"
-                        />
-                    </DescriptionContainer>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <ButtonWrapper>
-                        <CallToAction>Create post</CallToAction>
-                    </ButtonWrapper>
-                </Col>
-            </Row>
-        </Container>
-    )
+  const onSubmit = async () => {
+    const result = await submitFunction(payload);
+    responseHandler(result, payload);
+    return result;
+  };
+
+  return (
+    <Container>
+      <Navbar />
+      <Row>
+        <Col>
+          <HeadingContainer>
+            <HeadingText>
+              Hey, we're{" "}
+              <HeaderInput
+                value={post_author}
+                name={"post_author"}
+                onChange={handleInputChange}
+                placeholder="Name or Company name"
+                size={post_author.length}
+                maxLength={20}
+              />
+              And we are looking for{" "}
+              <HeaderInput
+                value={target_audience}
+                name={"target_audience"}
+                onChange={handleInputChange}
+                size={target_audience.length}
+                placeholder="designer, developer, artists..."
+                maxLength={20}
+              />
+              for our{" "}
+              <HeaderInput
+                value={idea_description}
+                name={"idea_description"}
+                onChange={handleInputChange}
+                size={idea_description.length}
+                placeholder="your idea I.E our charity"
+                maxLength={20}
+              />
+            </HeadingText>
+          </HeadingContainer>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <DescriptionContainer>
+            <DescriptionInput
+              value={long_text}
+              name={"idea_description"}
+              onChange={handleInputChange}
+              cols={40}
+              rows={5}
+              placeholder="Add any additional information"
+            />
+          </DescriptionContainer>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <ButtonWrapper>
+            <CallToAction onClick={onSubmit}>Create post</CallToAction>
+          </ButtonWrapper>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default PostForm;
